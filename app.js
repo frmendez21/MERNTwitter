421b7fbde7432a4005408e47a690c4cf3150d23f
frmendez21 <frmendez209@gmail.com>
2021-04-18 23:54:13 -0700

    mern twitter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const passport = require('passport');
+const path = require('path');
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to mongoDB"))
